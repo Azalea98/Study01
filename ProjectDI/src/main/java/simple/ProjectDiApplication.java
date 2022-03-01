94ede006beb13f02877760d7c2964bddf3e72b7e
Azalea98 <dlwjddls9@naver.com>
2022-03-01 23:33:38 +0900

    될까???

--- ProjectDI/src/main/java/simple/ProjectDiApplication.java
+++ ProjectDI/src/main/java/simple/ProjectDiApplication.java
@@ -8,7 +8,7 @@ public class ProjectDiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectDiApplication.class, args);
-		System.out.println();
+		System.out.println("?????");
 	}
 
 }
